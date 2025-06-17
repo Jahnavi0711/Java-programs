fc4e15eab8b431dc3cf36dc291fb1ba037ad2c37
Jahnavi0711 <gajulapallijahnavi24@gmail.com>
2025-06-17 17:28:37 +0530

    Add files via upload

---
+++ static input using loop.java
@@ -0,0 +1,12 @@
+//static input using loop
+
+public class Main
+{
+	public static void main(String[] args) {
+		String a=new String("aswini naidu");
+		for(int i=0;i<a.length();i++)
+		{
+		System.out.println(a);
+		}
+	}
+}
