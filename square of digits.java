c457b546e5142a9d7e62960ce8fb2238800862c9
Jahnavi0711 <gajulapallijahnavi24@gmail.com>
2025-06-17 17:27:21 +0530

    Add files via upload

---
+++ square of digits.java
@@ -0,0 +1,17 @@
+//square of digits
+import java.util.*;
+public class Main {
+    public static void main(String[] args) 
+    {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int sum=0;
+        while (n != 0) 
+        {
+            int rem = n % 10;
+            sum=sum+rem*rem;
+            n=n/10;
+        }
+        System.out.println(+sum);
+    }
+}
